c378ca8cde66f77c5366568e3a105239e509ecec
Marius Storhaug <marstor@hotmail.com>
2025-02-09 18:08:20 +0100

    🩹 [Patch]: Update context test descriptions to use 'Get-Context' for improved clarity and consistency; enhance pipeline input support assertions

--- tests/Context.Tests.ps1
+++ tests/Context.Tests.ps1
@@ -18,7 +18,7 @@ BeforeAll {
 }
 
 Describe 'Functions' {
-    Context 'Function: Set-Context' {
+    Get-Context 'Function: Set-Context' {
         It "Set-Context -ID 'TestID1'" {
             { Set-Context -ID 'TestID1' } | Should -Not -Throw
             $result = Get-Context -ID 'TestID1'
@@ -152,7 +152,7 @@ Describe 'Functions' {
         # }
     }
 
-    Context 'Function: Get-Context' {
+    Get-Context 'Function: Get-Context' {
         It 'Get-Context - Should return all contexts' {
             Write-Verbose (Get-Context | Out-String) -Verbose
             (Get-Context).Count | Should -Be 3
@@ -177,7 +177,7 @@ Describe 'Functions' {
         }
     }
 
-    Context 'Function: Remove-Context' {
+    Get-Context 'Function: Remove-Context' {
         It "Remove-Context -ID 'AContextID' - Should remove the context" {
             Get-Context | Remove-Context
 
@@ -205,7 +205,7 @@ Describe 'Functions' {
         }
     }
 
-    Context 'Function: Rename-Context' {
+    Get-Context 'Function: Rename-Context' {
         BeforeEach {
             # Ensure no contexts exist before starting tests
             Get-Context | Remove-Context
@@ -256,7 +256,7 @@ Describe 'Functions' {
     }
 
     # New tests to verify that pipeline input is fully supported
-    Context 'Pipeline Input support' {
+    Get-Context 'Pipeline Input support' {
         It 'Get-Context supports pipeline input as strings' {
             # Create two contexts to test pipeline input
             Set-Context -ID 'PipeContext1' -Context @{ Dummy = 1 }
@@ -284,7 +284,7 @@ Describe 'Functions' {
             $result | Should -Not -BeNullOrEmpty
             $result.ID | Should -Contain 'PipeInfo1'
             $result.ID | Should -Contain 'PipeInfo2'
-            $result.Context | Should -BeNullOrEmpty
+            $result.PSObject.Properties.Name | Should -Not -Contain Context
         }
 
         It 'Get-ContextInfo supports pipeline input by property name' {
@@ -292,9 +292,10 @@ Describe 'Functions' {
             Set-Context -ID 'PipeInfo3' -Context @{ Dummy = 3 }
             $obj = [PSCustomObject]@{ ID = 'PipeInfo3' }
             $result = $obj | Get-ContextInfo
+            $result | Should -
             $result | Should -Not -BeNullOrEmpty
             $result.ID | Should -Be 'PipeInfo3'
-            $result.Context | Should -BeNullOrEmpty
+            $result.PSObject.Properties.Name | Should -Not -Contain Context
         }
     }
 }
