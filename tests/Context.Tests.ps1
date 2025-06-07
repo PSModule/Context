e6148ef293e823c5b2f0caa6020d12e9a9b596e5
Marius Storhaug <marstor@hotmail.com>
2025-06-07 19:51:05 +0200

    Refactor test files: update suppression messages for clarity and consistency in Context.Tests.ps1 and ContextVaults.Tests.ps1

--- tests/Context.Tests.ps1
+++ tests/Context.Tests.ps1
@@ -1,5 +1,20 @@
-﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
-    'PSAvoidUsingConvertToSecureStringWithPlainText', '', Justification = 'Test code only'
+﻿#Requires -Modules @{ ModuleName = 'Pester'; RequiredVersion = '5.7.1' }
+
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSUseDeclaredVarsMoreThanAssignments', '',
+    Justification = 'Pester grouping syntax: known issue.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingConvertToSecureStringWithPlainText', '',
+    Justification = 'Used to create a secure string for testing.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingWriteHost', '',
+    Justification = 'Log outputs to GitHub Actions logs.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidLongLines', '',
+    Justification = 'Long test descriptions and skip switches'
 )]
 [CmdletBinding()]
 param()
