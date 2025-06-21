89400842d510190b93ae41329abbbbf27f8c63c0
Marius Storhaug <marstor@hotmail.com>
2025-06-21 23:45:42 +0200

    🆕 [Add]: Implement ContextInfo class and its XML format configuration

---
+++ src/classes/public/ContextInfo.ps1
@@ -0,0 +1,15 @@
+﻿class ContextInfo {
+    [string] $ID
+    [string] $Path
+    [string] $Vault
+    [string] $Context
+
+    ContextInfo() {}
+
+    ContextInfo([PSCustomObject]$Object) {
+        $this.ID = $Object.ID
+        $this.Path = $Object.Path
+        $this.Vault = $Object.Vault
+        $this.Context = $Object.Context
+    }
+}
